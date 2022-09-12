d2abfab25f88c761913662b79fc6fa4b697f74f5
rafa-Nobre <denys.nobre@outlook.com>
2022-09-12 14:39:05 -0300

    Implementando Middleware Thunk

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,28 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import MovieListing from '../MovieListing/MovieListing'
-import movieApi from '../../common/apis/movieApi'
-import { APIKey } from '../../common/apis/MovieApiKey'
 import './Home.scss'
 import { useDispatch } from 'react-redux'
-import { addMovies } from '../../features/movies/movieSlice'
+import { fetchAsyncMovies } from '../../features/movies/movieSlice'
 
 const Home = () => {
-  const movieText = 'Harry'
   const dispatch = useDispatch()
   //const [movieData, setMovieData] = useState([])
   useEffect(() => {
-    const fetchMovies = async () => {
-      const response = await movieApi
-        .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
-        .catch(err => {
-          console.log('Err: ', err)
-        })
-      dispatch(addMovies(response.data))
-      //setMovieData(response.data.Search)
-    }
-
-    fetchMovies()
-  }, [])
+    dispatch(fetchAsyncMovies())
+  }, [dispatch])
   return (
     <div>
       <div className="banner-img"></div>
